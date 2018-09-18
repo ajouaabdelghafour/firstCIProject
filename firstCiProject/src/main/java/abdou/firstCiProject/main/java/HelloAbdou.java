d3259711ed76cd2c6685d276330f15f36272bc2d
Abdelghafour AJOUA <ajoua.abdelghafour@gmail.com>
2018-09-18 22:41:44 +0100

    HelloAbdou class added with its unit test

---
+++ firstCiProject/src/main/java/abdou/firstCiProject/main/java/HelloAbdou.java
@@ -0,0 +1,15 @@
+package abdou.firstCiProject.main.java;
+
+public class HelloAbdou {
+
+	public static void main(String[] args) {
+		
+		System.out.println(new HelloAbdou().hello());
+
+	}
+
+	public String hello() {
+		return "Hello Mr. Abdelghafour AJOUA";
+	}
+
+}
